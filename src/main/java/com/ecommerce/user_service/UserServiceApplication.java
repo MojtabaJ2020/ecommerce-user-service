6e060ba3a709fbf88466c6d0530ee18c93fb24f9
Mojtaba Jamshidi <mojtaba.Jamshidi@boc-group.com>
2025-01-29 14:40:11 +0100

    Added eureka dependency

--- src/main/java/com/ecommerce/user_service/UserServiceApplication.java
+++ src/main/java/com/ecommerce/user_service/UserServiceApplication.java
@@ -4,9 +4,10 @@ import com.ecommerce.user_service.property.EncryptionProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableConfigurationProperties (EncryptionProperties.class)
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
